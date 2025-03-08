9fe1fd705e1b347dcd75ad9aecb72b78bcc18236
Matt Herold <mattherold@icloud.com>
2025-03-08 17:03:27 +0100

    Added writing datasets.

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -7,9 +7,10 @@
 #include <chrono>
 
 #include <arrow/api.h>
-#include <arrow/compute/api.h>  // for Arrow's compute APIs
-#include <arrow/acero/api.h>     // for Acero APIs
 
+#include <arrow/compute/api.h>
+#include <arrow/acero/api.h>
+#include <arrow/dataset/api.h>
 
 namespace ac = arrow::acero;
 namespace cp = arrow::compute;
@@ -34,7 +35,6 @@ arrow::Result<std::shared_ptr<arrow::RecordBatch>> CreateSampleBatch() {
         ARROW_RETURN_NOT_OK(stringBuilder.Append(groups[rand() % std::size(groups)]));
         ARROW_RETURN_NOT_OK(intBuilder.Append(i));
     }
-    // AppendValues, as called, puts 5 values from days_raw into our Builder object.
     
     // We only have a Builder though, not an Array -- the following code pushes out the
     // built up data into a proper Array.
@@ -56,20 +56,19 @@ arrow::Result<std::shared_ptr<arrow::RecordBatch>> CreateSampleBatch() {
     // each column is internally contiguous. This is in opposition to Tables, which we'll
     // see next.
     std::shared_ptr<arrow::RecordBatch> rbatch;
+    
     // The RecordBatch needs the schema, length for columns, which all must match,
     // and the actual data itself.
     rbatch = arrow::RecordBatch::Make(schema, values->length(), {group, values});
     
-    
     return arrow::Result<std::shared_ptr<arrow::RecordBatch>>(rbatch);
 }
-
 
 arrow::Result<std::shared_ptr<arrow::RecordBatchReader>> CreateRecordBatchReader() {
     std::shared_ptr<arrow::RecordBatchReader> reader;
     std::vector<std::shared_ptr<arrow::RecordBatch>> batches;
     
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 100000; i++) {
         std::shared_ptr<arrow::RecordBatch> rbatch;
         ARROW_ASSIGN_OR_RAISE(rbatch, CreateSampleBatch());
         batches.push_back(rbatch);
@@ -80,18 +79,13 @@ arrow::Result<std::shared_ptr<arrow::RecordBatchReader>> CreateRecordBatchReader
     return arrow::Result<std::shared_ptr<arrow::RecordBatchReader>>(reader);
 }
 
-
-
 ac::Declaration RecordBatchSourceNode(std::shared_ptr<arrow::RecordBatchReader> reader) {
     auto source_node_options = ac::RecordBatchReaderSourceNodeOptions{reader};
-    std::cout << "Source" << std::endl;
     
     ac::Declaration source{"record_batch_reader_source", std::move(source_node_options)};
-     
+    
     return source;
 }
-
-
 
 ac::Declaration CalcQuantileNode(ac::Declaration previousNode, double quantile) {
     auto options = std::make_shared<cp::CountOptions>(cp::CountOptions::ONLY_VALID);
@@ -131,13 +125,8 @@ ac::Declaration GetValuesWithCountLargerThanNode(ac::Declaration previousNode, d
 }
 
 ac::Declaration FilterByValueSet(ac::Declaration previousNode, arrow::Datum valueSet) {
-//    std::shared_ptr<arrow::Field> field_group = arrow::field("Group", arrow::utf8());
-//    std::shared_ptr<Node> field_x = arrow::gandiva::TreeExprBuilder::MakeField(field_x_raw);
-
-    
     auto quantile_options = std::make_shared<cp::SetLookupOptions>(arrow::Datum(valueSet));
-
-  //  cp::Expression filter_expr = cp::equal(cp::field_ref("Group"), cp::literal(valueSet));
+    
     cp::Expression filter_expr = cp::call("is_in", std::vector<cp::Expression>{
         cp::field_ref("Group")
     }, quantile_options);
@@ -154,32 +143,70 @@ arrow::Result<std::shared_ptr<arrow::Table>> ExecutePlanToTable(ac::Declaration 
     
     std::shared_ptr<arrow::Table> table;
     ARROW_ASSIGN_OR_RAISE(table, ac::DeclarationToTable(previousNode));
-
-    return table;
     
+    return table;
+}
+
+arrow::Status ExecutePlanToDataset(ac::Declaration previousNode, std::string dataset_path) {
+    std::string root_path;
+    
+    ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::fs::FileSystem> filesystem,
+                          arrow::fs::FileSystemFromUri(dataset_path, &root_path));
+    
+    auto set_path = root_path;
+    std::cout << "Writing dataset to " << set_path << std::endl;
+    
+    // ARROW_RETURN_NOT_OK(filesystem->DeleteDirContents(base_path));
+    ARROW_RETURN_NOT_OK(filesystem->CreateDir(set_path));
+    
+    auto partition_schema = arrow::schema({arrow::field("Group", arrow::utf8())});
+    
+    auto partitioning =
+    std::make_shared<arrow::dataset::HivePartitioning>(partition_schema);
+    
+    // We'll write Parquet files.
+    auto format = std::make_shared<arrow::dataset::ParquetFileFormat>();
+    
+    arrow::dataset::FileSystemDatasetWriteOptions write_options;
+    write_options.file_write_options = format->DefaultWriteOptions();
+    write_options.filesystem = filesystem;
+    write_options.base_dir = set_path;
+    write_options.partitioning = partitioning;
+    write_options.basename_template = "part{i}.parquet";
+    write_options.existing_data_behavior = arrow::dataset::ExistingDataBehavior::kOverwriteOrIgnore;
+    arrow::dataset::WriteNodeOptions write_node_options{write_options};
+    
+    std::shared_ptr<arrow::RecordBatchReader> reader;
+    ARROW_ASSIGN_OR_RAISE(reader, ac::DeclarationToReader(previousNode));
+    
+    auto write_scanner_builder =
+    arrow::dataset::ScannerBuilder::FromRecordBatchReader(reader);
+    ARROW_ASSIGN_OR_RAISE(auto write_scanner, write_scanner_builder->Finish());
+    
+    ARROW_RETURN_NOT_OK(
+                        arrow::dataset::FileSystemDataset::Write(write_options, write_scanner));
+    
+    std::cout << "Dataset written to " << set_path << std::endl;
+    
+    return arrow::Status::OK();
 }
 
 arrow::Result<std::shared_ptr<arrow::DoubleScalar>> TableToDoubleScalar(std::shared_ptr<arrow::Table> table) {
-        // Access the first column (index 0) from the Table
-        std::shared_ptr<arrow::ChunkedArray> column = table->column(0);  // First column (int_column)
-        
-        std::shared_ptr<arrow::Scalar> scalar;
-        ARROW_ASSIGN_OR_RAISE(scalar, column->GetScalar(0));
-        
-        return arrow::Result(std::dynamic_pointer_cast<arrow::DoubleScalar>(scalar));
+    // Access the first column (index 0) from the Table
+    std::shared_ptr<arrow::ChunkedArray> column = table->column(0);  // First column (int_column)
+    
+    std::shared_ptr<arrow::Scalar> scalar;
+    ARROW_ASSIGN_OR_RAISE(scalar, column->GetScalar(0));
+    
+    return arrow::Result(std::dynamic_pointer_cast<arrow::DoubleScalar>(scalar));
 }
 
 
-arrow::Result<std::shared_ptr<arrow::Array>> TableToArray(std::shared_ptr<arrow::Table> table) {
+arrow::Result<std::shared_ptr<arrow::ChunkedArray>> TableToArray(std::shared_ptr<arrow::Table> table) {
     // Access the first column (index 0) from the Table
     std::shared_ptr<arrow::ChunkedArray> valuesColumn = table->column(0);  // First column
-
-    std::shared_ptr<arrow::Array> flattened;
     
-    if (valuesColumn->num_chunks() > 1) {
-    }
-    
-    return arrow::Result<std::shared_ptr<arrow::Array>>(valuesColumn->chunk(0));
+    return valuesColumn;
 }
 
 arrow::Status WriteBatches(std::shared_ptr<arrow::RecordBatchReader> reader) {
@@ -205,76 +232,97 @@ arrow::Status RunMain() {
     auto endTime = startTime;
     std::chrono::milliseconds duration;
     
-    std::cout << "Calculating quantile" << std::endl;
-
+    std::cout << "Calculating quantile..." << std::endl;
+    
     std::shared_ptr<arrow::RecordBatchReader> reader;
     ARROW_ASSIGN_OR_RAISE(reader, CreateRecordBatchReader());
-
+    
     ac::Declaration sourceNode = RecordBatchSourceNode(reader);
-    ac::Declaration calcQuantileNode = CalcQuantileNode(sourceNode, 0.9);
+    ac::Declaration calcQuantileNode = CalcQuantileNode(sourceNode, 0.995);
     
     std::shared_ptr<arrow::Table> table;
     ARROW_ASSIGN_OR_RAISE(table, ExecutePlanToTable(calcQuantileNode));
-
+    
     std::shared_ptr<arrow::DoubleScalar> quantile;
     ARROW_ASSIGN_OR_RAISE(quantile, TableToDoubleScalar(table));
-
+    
     std::cout << "Calculated quantile..." << std::endl;
     std::cout << quantile->ToString() << std::endl;
- 
+    
     /* Measure timing */
     endTime = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
     std::cout << "-- Execution duration: " << duration.count() << "ms\n";
     startTime = std::chrono::high_resolution_clock::now();
     /* Measure timing */
-
+    
     /*
      * Filter values with count larger than 10
      */
+    std::cout << "Building exclude group list..." << std::endl;
+
     std::shared_ptr<arrow::RecordBatchReader> reader2;
     ARROW_ASSIGN_OR_RAISE(reader2, CreateRecordBatchReader());
-
+    
     ac::Declaration sourceNode2 = RecordBatchSourceNode(reader2);
     ac::Declaration valuesLargerThanNode = GetValuesWithCountLargerThanNode(sourceNode2, quantile->value);
     
     std::shared_ptr<arrow::Table> table2;
     ARROW_ASSIGN_OR_RAISE(table2, ExecutePlanToTable(valuesLargerThanNode));
-
-    std::shared_ptr<arrow::Array> array;
+    
+    std::shared_ptr<arrow::ChunkedArray> array;
     ARROW_ASSIGN_OR_RAISE(array, TableToArray(table2));
-
-    std::cout << "Group values: " << std::endl;
+    
+    std::cout << "Excluded groups: " << std::endl;
     std::cout << array->ToString() << std::endl;
-
+    
     /* Measure timing */
     endTime = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
     std::cout << "-- Execution duration: " << duration.count() << "ms\n";
     startTime = std::chrono::high_resolution_clock::now();
     /* Measure timing */
-
+    
     /*
      * Filter original list
      */
+    std::cout << "Filtering orginal list..." << std::endl;
+
     std::shared_ptr<arrow::RecordBatchReader> reader3;
     ARROW_ASSIGN_OR_RAISE(reader3, CreateRecordBatchReader());
-
+    
     ac::Declaration sourceNode3 = RecordBatchSourceNode(reader3);
     ac::Declaration valueSetFilter = FilterByValueSet(sourceNode3, array);
     
     std::shared_ptr<arrow::Table> table3;
     ARROW_ASSIGN_OR_RAISE(table3, ExecutePlanToTable(valueSetFilter));
-
-    std::cout << "Results" << std::endl;
+    
+    std::cout << "Final results" << std::endl;
     std::cout << table3->ToString() << std::endl;
- 
+    
     /* Measure timing */
     endTime = std::chrono::high_resolution_clock::now();
     duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
     std::cout << "-- Execution duration: " << duration.count() << "ms\n";
     /* Measure timing */
-
+    
+    /*
+     * Filter and write dataset
+     */
+    std::shared_ptr<arrow::RecordBatchReader> reader4;
+    ARROW_ASSIGN_OR_RAISE(reader4, CreateRecordBatchReader());
+    
+    ac::Declaration sourceNode4 = RecordBatchSourceNode(reader4);
+    ac::Declaration valueSetFilter2 = FilterByValueSet(sourceNode4, array);
+    
+    ARROW_RETURN_NOT_OK(ExecutePlanToDataset(valueSetFilter2, "file:///Users/herold/Desktop/test/parquet"));
+    
+    /* Measure timing */
+    endTime = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+    std::cout << "-- Execution duration: " << duration.count() << "ms\n";
+    /* Measure timing */
+    
     return arrow::Status::OK();
 }
 
